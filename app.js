8d4e25ed0d557c6c2c953ad367fbb21e6d77216b
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2019-10-27 10:59:12 -0700

    express.js Install

--- app.js
+++ app.js
@@ -1,50 +1,20 @@
-//Packege Import
 const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-//Server Creation
-const server = http.createServer((req,res) => {
-    //console.log(req.url, req.method, req.headers);
-    //process.exit();
-    //Response Handaling
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Hello World</title></head>');
-        res.write('<body><form action="/message" method = "POST"><input type="text" name="txt"><input type="submit"></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if(url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
+const app = express();
 
-        req.on('end',()=>{
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFile('message.txt',message,(err)=>{
-                res.statusCode = 302;
-                res.setHeader('Location','/');
-                return res.end();
-            });
-
-        });
-        
-        
-    }
-
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Hello World</title></head>');
-    res.write('<body><h1>Welcome From my node.js Server</h1></body>');
-    res.write('</html>');
-    res.end();
-
+app.use((req, res, next) => {
+    console.log("This is From Middlewire");
+    next();
 });
 
-server.listen(5000);
+app.use((req, res, next) => {
+    res.send('<h1>Welcome From Express.Js</h1>');
+    next();
+});
+
+
+
+const server = http.createServer(app);
+
+server.listen(3000);
--- introduction.js
+++ introduction.js
@@ -1,6 +1,11 @@
+import { inherits } from "util"
 
 
 Md Sabbir Hossain
 Software Engineer 
 Promiti Computer And Network (PVT) Ltd 
 23/10/2019
+
+1. npm init 
+2. npm install nodemon --save-dev 
+3. npm install --save express
